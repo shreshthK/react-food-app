202e5eee3b66b2090b055688f66303534d1d9a85
shreshth kandari <>
2018-08-07 17:30:15 -0500

    Project setup

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -182,7 +182,9 @@ module.exports = {
                       options: {
                         importLoaders: 1,
                         minimize: true,
+                        modules: true,
                         sourceMap: shouldUseSourceMap,
+                        localIdentName: '[name]_[local]_[hash:base64:5]'
                       },
                     },
                     {
